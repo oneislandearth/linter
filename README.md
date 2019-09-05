27ec7f6880695808e425c45393838b87d68d440c
larkin-nz <daniel@larkin.nz>
2019-09-05 12:27:59 +0800

    First release

---
+++ README.md
@@ -0,0 +1,21 @@
+<p align="center">
+  <a href="https://github.com/oneislandearth/linter" target="_blank">
+    <img src=https://i.imgur.com/bqIMZaR.png">
+  </a>
+</p>
+
+***
+
+A simple linter used for OneIsland projects
+
+## Usage
+
+Simply install the package in the required project
+
+Linter will automatically create symbolic links to the required configuration files
+
+## Installation
+
+```bash
+npm install @oneisland/linter
+```
---
+++ postinstall.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+/* Post-install script for the linter */
+
+// Import the node modules
+const fs = require('fs');
+const path = require('path');
+
+// Create the symbolic link to a file
+const createLink = (file) => {
+
+  // Resolve the paths for the file
+  const package = path.resolve(__dirname, file);
+  const project = path.resolve(process.env.INIT_CWD, file);
+
+  // Check that the file does not exists in the project folder
+  if (!fs.existsSync(package)) {
+
+    // Create a symbolic link to the file in the project folder
+    fs.symlinkSync(package, project);
+  }
+}
+
+// Create the symbolic links to the files
+createLink('.eslintignore');
+createLink('.eslintrc.js');
